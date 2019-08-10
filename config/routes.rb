996a739a5d49290ead2ee7f8b0f78ec55a3c0e2a
Kirill Mokevnin <mokevnin@gmail.com>
2019-08-10 00:20:17 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   scope module: :web do
     root 'home#index'
+    resources :resumes do
+      scope module: :resumes do
+        resources :answers
+      end
+    end
+    resources :answers, only: [] do
+      scope module: :answers do
+        resources :comments
+      end
+    end
   end
 end
