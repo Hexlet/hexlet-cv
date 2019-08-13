971d807c3bce736d4c3223b3fbd41709342e7a6a
Vadim Safonov <imamatory@gmail.com>
2019-08-14 00:09:02 +0300

    Fix type in module name

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  include AuthManagment
+  include AuthManagement
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
