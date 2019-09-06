dc39e412a7034322f4150de86872bd50cd4dd746
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-05 23:28:06 -0400

    add fixme to resumes_controller

--- app/controllers/web/resumes_controller.rb
+++ app/controllers/web/resumes_controller.rb
@@ -2,6 +2,7 @@
 
 class Web::ResumesController < ApplicationController
   def show
+    # FIXME: check if user already rated answers
     @resume = Resume.find(params[:id])
     @answer = Resume::Answer.new resume: @resume
     @current_user_answer = @resume.answers.find_by(user: current_user)
