64700cef749dd89abfe6ed3f3ac59fde6c3c77e0
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-07 12:32:19 -0400

    add validations to resume associations

--- app/models/resume/education.rb
+++ app/models/resume/education.rb
@@ -2,4 +2,8 @@
 
 class Resume::Education < ApplicationRecord
   belongs_to :resume, dependent: :destroy
+
+  validates :institution, presence: true
+  validates :degree, presence: true
+  validates :start_date, presence: true
 end
--- app/models/resume/work.rb
+++ app/models/resume/work.rb
@@ -2,4 +2,9 @@
 
 class Resume::Work < ApplicationRecord
   belongs_to :resume, dependent: :destroy
+
+  validates :compnay, presence: true
+  validates :position, presence: true
+  validates :start_date, presence: true
+  validates :description, presence: true
 end
