97f2de06caff0206cee13850671bed4515be224a
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-07 18:17:29 -0400

    improve validation for resume/answer

--- app/models/resume/answer.rb
+++ app/models/resume/answer.rb
@@ -3,10 +3,9 @@
 class Resume::Answer < ApplicationRecord
   # FIXME: add unique index
   validates :resume, uniqueness: { scope: :user }
+  validates :content, presence: true, length: { minimum: 200 }
 
   belongs_to :resume
   belongs_to :user
   has_many :likes, foreign_key: 'resume_answer_id', dependent: :delete_all, inverse_of: :answer
-
-  validates :content, presence: true
 end
