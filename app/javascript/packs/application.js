32510e2fcd294b9b4ee859fbbfb01e14a40da234
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-07 11:53:14 -0400

    add cocoon js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('jquery')
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require('./cocoon')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
 environment.config.externals = {
-  gon: 'gon'
-}
+  gon: 'gon',
+};
 
-module.exports = environment
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+  }),
+);
+
+module.exports = environment;
