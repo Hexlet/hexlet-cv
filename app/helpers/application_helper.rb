654d5ab6f0fd589f2d01d5bdee370af472a292d1
greybutton <greybutton@yandex.com>
2019-09-07 22:20:03 +0400

    add navbar helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,22 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
     markdown.render(text)
   end
+
+  def active?(path, options = {})
+    # raise options.inspect
+    if options.key? :active_if
+      'active' if options[:active_if]
+    elsif current_page?(path)
+      'active'
+    end
+  end
+
+  def navbar_menu_item(name, path = '#')
+    options = { class: "nav-link #{active?(path)}" }
+    content_tag :li, class: 'nav-item' do
+      link_to path, options do
+        content_tag(:div, name)
+      end
+    end
+  end
 end
