eef8e7f565da098a29017074fce9f5e734d0eab9
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-07 17:51:04 -0400

    add bootstrap js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require('jquery')
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-require('./cocoon')
+require('jquery');
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
+require('popper.js');
+require('bootstrap');
+require('./cocoon');
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
