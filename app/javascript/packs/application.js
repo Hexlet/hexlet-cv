3cf56c07de9b07a11045f233ccec3c991ea14301
Alexander Pashchenko <point212@gmail.com>
2019-09-11 01:34:45 +0300

    move application.scss under webpacker control

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require('popper.js');
 require('bootstrap');
 require('./cocoon');
 
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
