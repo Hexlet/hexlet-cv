a7ca8dd9a8b8d00bd9c78977beab50ebc87de93a
moklidia <kitsunegakkou@gmail.com>
2019-09-11 22:55:28 +0400

    config markdown to open links in new tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,8 @@ module ApplicationHelper
       filter_html: true,
       safe_links_only: true
     }
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions)
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, link_attributes: { target: '_blank' })
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text)
   end
 
