2d83beffef72c2797d6b2c6c183572d41242ae0b
greybutton <greybutton@yandex.com>
2019-09-11 22:19:49 +0400

    refactor users show

--- app/controllers/web/users_controller.rb
+++ app/controllers/web/users_controller.rb
@@ -3,8 +3,8 @@
 class Web::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_answers = Resume::Answer.where(user: @user)
-    @user_likes = @user_answers.reduce(0) { |sum, answer| sum + answer.likes.count }
-    @user_resumes = Resume.web.where(user: @user)
+    @user_answers = @user.resume_answers
+    @user_likes = @user.resume_answers.sum('likes_count')
+    @user_resumes = @user.resumes
   end
 end
