250909bc87854a61fe09a47088f5c414ce07e635
Oleksandr Ulizko <ulizman@gmail.com>
2019-09-15 12:06:33 +0300

    fix production fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile << /.(?:svg|eot|woff|woff2|ttf)$/
+
