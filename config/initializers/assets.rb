6fcbe66d221cbcc48f8e34b94a1ece4dc10a7ddf
Oleksandr Ulizko <ulizman@gmail.com>
2019-09-16 14:41:55 +0300

    remove trailing blank line

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,4 +15,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile << /.(?:svg|eot|woff|woff2|ttf)$/
-
