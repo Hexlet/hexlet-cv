4e783daed5422aba1cd52cc99ed1465e73d541fc
Oleksandr Ulizko <ulizman@gmail.com>
2019-09-21 07:15:14 +0300

    add test for exist user

--- test/controllers/web/omniauth_controller_test.rb
+++ test/controllers/web/omniauth_controller_test.rb
@@ -3,9 +3,15 @@
 require 'test_helper'
 
 class Web::OmniauthControllerTest < ActionDispatch::IntegrationTest
-  test '#github' do
+  test '#github (new user)' do
     sign_with_github
     assert_response :redirect
     assert { User.exists?(email: 'github@github.com') }
+  end
+
+  test '#github (exist user)' do
+    user = users(:with_github)
+    sign_with_github
+    assert_response :redirect
   end
 end
