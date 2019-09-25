571d42761bb6e5002428e5d800bbcebab9bb0466
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-24 21:21:18 -0400

    remove requiring github vars

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, ENV.fetch('GITHUB_APP_ID'), ENV.fetch('GITHUB_APP_SECRET'), scope: 'user'
+  config.omniauth :github, ENV.fetch('GITHUB_APP_ID', 'fake'), ENV.fetch('GITHUB_APP_SECRET', 'fake'), scope: 'user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+OmniAuth.config.test_mode = true
+OmniAuth.config.add_mock(
+  :github,
+  provider: 'github',
+  uid: '12345',
+  info: { name: 'Github User', email: 'github@github.com' }
+)
+Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   # TODO: return virtualization after fixing power_assert integration
@@ -18,29 +27,7 @@ end
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  OmniAuth.config.test_mode = true
-  OmniAuth.config.add_mock(
-    :github,
-    provider: 'github',
-    uid: '12345',
-    info: { name: 'Github User', email: 'github@github.com' }
-  )
-
   def sign_with_github
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
     get user_github_omniauth_callback_path
   end
-
-  # def login(user)
-  #   params = {
-  #     new_user: {
-  #       email: user.email,
-  #       password: 'password',
-  #       password_confirmation: 'password'
-  #     }
-  #   }
-  #   open_session do |s|
-  #     s.post s.user_session_url, params: params
-  #   end
-  # end
 end
