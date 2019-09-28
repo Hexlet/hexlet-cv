8bf7ddc3857102b27a2df6418cbecc3eeaddfe0b
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2019-09-28 23:30:59 +0500

    add links with anchors to notifications

--- app/lib/notifications_helper.rb
+++ app/lib/notifications_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class NotificationsHelper
+  include Rails.application.routes.url_helpers
+
   attr_reader :notification
 
   def initialize(notification)
@@ -11,24 +13,40 @@ class NotificationsHelper
 
   def message
     params = send("#{kind}_params")
-    I18n.t(kind, scope: [:notifications], **params)
+    I18n.t("#{kind}_html", scope: [:notifications], **params).html_safe
   end
 
   private
 
   def new_answer_params
-    { user: resource.user }
+    {
+      user: resource.user,
+      user_path: user_path(resource.user),
+      answer_path: resume_path(resource.resume, anchor: "answer#{resource.id}")
+    }
   end
 
   def new_comment_params
-    { user: resource.user }
+    {
+      user: resource.user,
+      user_path: user_path(resource.user),
+      comment_path: resume_path(resource.resume, anchor: "comment#{resource.id}")
+    }
   end
 
   def new_answer_comment_params
-    { user: resource.user }
+    {
+      user: resource.user,
+      user_path: user_path(resource.user),
+      answer_comment_path: resume_path(resource.resume, anchor: "answer_comment#{resource.id}")
+    }
   end
 
   def new_answer_like_params
-    { user: resource.user }
+    {
+      user: resource.user,
+      user_path: user_path(resource.user),
+      answer_path: resume_path(resource.resume, anchor: "answer#{resource.answer.id}")
+    }
   end
 end
