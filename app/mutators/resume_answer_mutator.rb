7f497b3dcca16b9395342b03e0fd504a797e5943
Kirill Mokevnin <mokevnin@gmail.com>
2019-09-28 18:11:59 -0400

    fix linter errors

--- app/mutators/resume_answer_mutator.rb
+++ app/mutators/resume_answer_mutator.rb
@@ -4,9 +4,7 @@ module ResumeAnswerMutator
   def self.create(resource_resume, params, current_user)
     answer = resource_resume.answers.build params
     answer.user = current_user
-    if answer.save
-      resource_resume.user.notifications.create!(kind: :new_answer, resource: answer)
-    end
+    resource_resume.user.notifications.create!(kind: :new_answer, resource: answer) if answer.save
 
     answer
   end
