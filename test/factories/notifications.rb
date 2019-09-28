c62fdc789218dfe00de2476aae38092d39a6c8b4
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2019-09-28 22:28:46 +0500

    fix lint

--- test/factories/notifications.rb
+++ test/factories/notifications.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :notification do
     user { nil }
     resourceable { nil }
-    state { "MyString" }
-    type { "" }
+    state { 'MyString' }
+    type { '' }
   end
 end
