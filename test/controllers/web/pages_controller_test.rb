cc7d21b1bb4bd89e02fb3a6048e950619bc4dfdf
greybutton <greybutton@yandex.com>
2019-10-06 22:20:14 +0400

    fix route about

--- test/controllers/web/pages_controller_test.rb
+++ test/controllers/web/pages_controller_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class Web::PagesControllerTest < ActionDispatch::IntegrationTest
   test 'should get about' do
-    get page_path(id: 'about')
+    get page_path('about')
     assert_response :success
   end
 end
