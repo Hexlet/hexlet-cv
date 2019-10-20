0e5c0af840563c00276a39a749daa5d9e959627f
greybutton <greybutton@yandex.com>
2019-10-20 19:18:25 +0400

    update mailer test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: ENV.fetch('PORT', 3000) }
+  config.action_mailer.deliver_later_queue_name = 'default_mailer_queue'
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
