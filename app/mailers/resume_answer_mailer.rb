086529889b938b9b8fbbe98361d95389c9203a55
greybutton <greybutton@yandex.com>
2019-10-20 22:13:46 +0400

    move mailer url to template

--- app/mailers/resume_answer_mailer.rb
+++ app/mailers/resume_answer_mailer.rb
@@ -3,9 +3,6 @@
 class ResumeAnswerMailer < ApplicationMailer
   def new_answer_email
     @answer = params[:answer]
-    @user_url = user_url(@answer.user)
-    @answer_url = resume_url(@answer.resume, anchor: "answer-#{@answer.id}")
-    @settings_url = account_profile_url(@answer.resume.user)
     mail(to: @answer.resume.user.email)
   end
 end
