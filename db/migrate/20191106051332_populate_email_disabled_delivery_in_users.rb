7ca817268935f1dbcd16fc61db9b60c04102c52f
greybutton <greybutton@yandex.com>
2019-11-27 09:34:01 +0400

    fix migration

--- db/migrate/20191106051332_populate_email_disabled_delivery_in_users.rb
+++ db/migrate/20191106051332_populate_email_disabled_delivery_in_users.rb
@@ -1,7 +1,7 @@
 class PopulateEmailDisabledDeliveryInUsers < ActiveRecord::Migration[6.0]
   def change
     User.find_each do |u|
-      u.email_disabled_delivery = User.bounced_email || User.marked_as_spam
+      u.email_disabled_delivery = u.bounced_email || u.marked_as_spam
       u.save!
     end
   end
