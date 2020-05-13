bbe4ad52342a91216d65f7083a5c7f6a43a68f91
plugin73 <laplugin73@gmail.com>
2020-05-13 14:35:22 +0000

    Revert "escape html"
    This reverts commit f5749fabaccba9dbeadab846c3cfb71dda7ee7be.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,13 +16,7 @@ module ApplicationHelper
       filter_html: true,
       safe_links_only: true
     }
-
-    options = {
-      escape_html: true,
-      hard_wrap: true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(options, link_attributes: { target: '_blank' })
+    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, link_attributes: { target: '_blank' })
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(text)
   end
