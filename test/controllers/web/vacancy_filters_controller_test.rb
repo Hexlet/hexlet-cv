1fa8a99ddc554d2e1c73cd8305f009e9623bbec5
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-15 20:14:15 +0400

    remove extra line
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- test/controllers/web/vacancy_filters_controller_test.rb
+++ test/controllers/web/vacancy_filters_controller_test.rb
@@ -3,7 +3,6 @@
 require 'test_helper'
 
 class Web::VacancyFiltersControllerTest < ActionDispatch::IntegrationTest
-
   test 'should get index' do
     get vacancy_filter_url('tag-php')
     assert_response :success
