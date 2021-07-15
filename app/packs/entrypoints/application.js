90764861a2e0915c1db2d1f8a9214dabe74397c8
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-15 13:24:53 +0400

    fix favicon
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -26,5 +26,7 @@ require('@nathanvda/cocoon');
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+
+// @ts-ignore
+const images = require.context('../images', true);
 // const imagePath = (name) => images(name, true)
