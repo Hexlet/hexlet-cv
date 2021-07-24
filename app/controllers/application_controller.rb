4a0d81fc1207d48f9daa8bd8dc14e5003f5fae51
Kirill Mokevnin <mokevnin@gmail.com>
2021-07-24 17:59:09 +0400

    fix linter errros
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
-require "application_responder"
+# frozen_string_literal: true
+
+require 'application_responder'
 
 # frozen_string_literal: true
 
--- app/views/web/vacancies/show.json.jbuilder
+++ app/views/web/vacancies/show.json.jbuilder
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 json.id @vacancy.id
 json.title @vacancy.title
 json.site @vacancy.site
