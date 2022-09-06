fdb3e3fa1be6a261fa5a7f366e0eab2a0fef5411
Alexander Illarionov <alex@heimstudio.ru>
2022-09-06 18:39:10 +0200

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_153127) do
+ActiveRecord::Schema.define(version: 2022_08_09_095608) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
@@ -211,6 +211,7 @@ ActiveRecord::Schema.define(version: 2022_02_17_153127) do
     t.boolean "email_disabled_delivery"
     t.string "role"
     t.string "state"
+    t.string "check_boxes"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
