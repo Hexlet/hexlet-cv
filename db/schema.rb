a4a2ce5ab0c13629d2ff6ac4b7dedfe8e6c8bfdf
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-04 13:38:31 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_18_143604) do
+ActiveRecord::Schema.define(version: 2022_11_27_083950) do
 
   create_table "countries", force: :cascade do |t|
     t.string "name"
