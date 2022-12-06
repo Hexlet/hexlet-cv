28be63e10da0248a3ff40a49bb7eb3f7c02d2f3e
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-06 04:35:25 +0300

    fix tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,15 +4,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test 'new user with default attribute resume_mail_enabled' do
-    attrs = {
-      first_name: 'Vasy',
-      last_name: 'Vetrov',
-      email: 'vasy@mail.ru',
-      password: '123456',
-      confirmed_at: Time.current,
-      role: :user,
-      state: 'permitted'
-    }
+    attrs = FactoryBot.attributes_for :user
 
     user = User.new(attrs)
     user.save!
