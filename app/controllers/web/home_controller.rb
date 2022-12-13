7461f7afbf7ceee89b4852ab07262333823a7131
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-13 12:40:21 +0300

    add filter by skill first_name last_name email resume_name

--- app/controllers/web/home_controller.rb
+++ app/controllers/web/home_controller.rb
@@ -2,9 +2,10 @@
 
 class Web::HomeController < Web::ApplicationController
   def index
-    q = Resume.web.ransack(params[:q])
-    @resumes = q.result(distinct: true).page(params[:page])
+    @q = Resume.web.ransack(params[:q])
+    @resumes = @q.result(distinct: true).includes(:user).page(params[:page])
     @page = params[:page]
+    @tags = Resume.tags_sorted_list
 
     set_meta_tags og: {
       description: t('.description'),
--- app/presenters/tag_vacancy_presenter.rb
+++ app/presenters/tag_vacancy_presenter.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module TagPresenter
+module TagVacancyPresenter
   def tags_sorted_list(sort_method = 'ASC')
     Vacancy.tags_on(:directions).order("lower(name) #{sort_method}").pluck(:name)
   end
