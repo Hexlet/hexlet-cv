a774f583f3f1c200bc5ee54931910c77ff54f7f0
mike09 <mike09@mail.ru>
2022-12-30 13:52:37 +0300

    Add goals links and descriptions

--- app/models/career.rb
+++ app/models/career.rb
@@ -3,18 +3,38 @@
 module Career
   GOALS = {
     'work_course' => {
-      link: 'https://ru.hexlet.io/courses/employment'
+      links: [
+        {
+          caption: 'Курс "Трудоустройство"',
+          link: 'https://ru.hexlet.io/courses/employment'
+        }
+      ]
     },
     'resume' => {},
     'letter' => {},
     'open_source' => {
-      link: 'https://ru.hexlet.io/blog/posts/kak-vybrat-svoy-pervyy-open-sors-proekt-instruktsiya-ot-heksleta'
+      links: [
+        {
+          caption: 'Как выбрать опен-сорс проект',
+          link: 'https://ru.hexlet.io/blog/posts/kak-vybrat-svoy-pervyy-open-sors-proekt-instruktsiya-ot-heksleta'
+        }
+      ]
     },
     'code_battle' => {
-      link: 'https://codebattle.hexlet.io/'
+      links: [
+        {
+          caption: "Codebattle by Hexlet's community",
+          link: 'https://codebattle.hexlet.io/'
+        }
+      ]
     },
     'hexlet_tests_repo' => {
-      link: 'https://github.com/Hexlet/ru-test-assignments'
+      links: [
+        {
+          link: 'https://github.com/Hexlet/ru-test-assignments',
+          caption: 'Список тестовых заданий для прокачки'
+        }
+      ]
     },
     'github_profile_review' => {},
     'interview' => {}
