a16606318c7779db8627d5cf4ecbb12751194939
askor26 <askorutin26@gmail.com>
2023-01-12 06:02:59 +0300

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -9,63 +9,63 @@ Rails.application.routes.draw do
                                     sessions: 'web/devise/sessions',
                                     registrations: 'web/devise/registrations' }
   scope '(:locale)', locale: /en|ru/ do
-    scope module: :web do
-      root 'home#index'
-      resource :locale, only: [] do
-        member do
-          get :switch
-        end
+    root 'home#index'
+  end
+  scope module: :web do
+    resource :locale, only: [] do
+      member do
+        get :switch
       end
+    end
 
-      resources :vacancies, only: %i[index show]
+    resources :vacancies, only: %i[index show]
 
-      # FIXME: фикс дирекшенов с точками типа node.js, убрать, когда определимся, нужно ли будем валидировать направления и запретим указывать точки
-      resources :vacancy_filters, only: %i[show], constraints: { id: %r{[^/]+} }, format: false, defaults: { format: 'html' } do
-        collection do
-          get :search, format: true
-        end
+    # FIXME: фикс дирекшенов с точками типа node.js, убрать, когда определимся, нужно ли будем валидировать направления и запретим указывать точки
+    resources :vacancy_filters, only: %i[show], constraints: { id: %r{[^/]+} }, format: false, defaults: { format: 'html' } do
+      collection do
+        get :search, format: true
       end
+    end
 
-      resources :resumes do
-        scope module: :resumes do
-          resources :answers do
-            member do
-              patch :change_applying_state
-            end
+    resources :resumes do
+      scope module: :resumes do
+        resources :answers do
+          member do
+            patch :change_applying_state
           end
-          resources :pdfs, only: :show
-          resources :comments
         end
+        resources :pdfs, only: :show
+        resources :comments
       end
-      resources :answers, only: [] do
-        scope module: :answers do
-          resources :comments
-          resources :likes, only: %i[create destroy]
-          resources :comments, only: %i[create update destroy]
-        end
+    end
+    resources :answers, only: [] do
+      scope module: :answers do
+        resources :comments
+        resources :likes, only: %i[create destroy]
+        resources :comments, only: %i[create update destroy]
       end
+    end
 
-      namespace :account do
-        resources :resumes
-        resources :vacancies
-        resources :notifications, only: %i[index update]
-        resource :newsletters, only: %i[edit update]
-        resource :profile, only: %i[edit update show]
-      end
+    namespace :account do
+      resources :resumes
+      resources :vacancies
+      resources :notifications, only: %i[index update]
+      resource :newsletters, only: %i[edit update]
+      resource :profile, only: %i[edit update show]
+    end
 
-      resources :users
-      resources :pages
+    resources :users
+    resources :pages
 
-      scope module: :hooks do
-        resource :sparkpost
-      end
+    scope module: :hooks do
+      resource :sparkpost
+    end
 
-      namespace :admin do
-        root 'home#index'
-        resources :users, only: %i[index edit update]
-        resources :resumes, only: %i[index edit update]
-        resources :vacancies, only: %i[index edit update]
-      end
+    namespace :admin do
+      root 'home#index'
+      resources :users, only: %i[index edit update]
+      resources :resumes, only: %i[index edit update]
+      resources :vacancies, only: %i[index edit update]
     end
   end
 end
