960ab27b8bfeda3af8972658904b7bba9fee309a
Andrey Groza <grozwalker@gmail.com>
2023-01-13 07:26:56 +0000

    fix: allow other host redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,6 @@ class ApplicationController < ActionController::Base
   def redirect_root_domain
     return if request.host == ENV.fetch('HOST')
 
-    redirect_to("#{request.protocol}#{ENV.fetch('HOST')}#{request.fullpath}", status: :moved_permanently)
+    redirect_to("#{request.protocol}#{ENV.fetch('HOST')}#{request.fullpath}", allow_other_host: true, status: :moved_permanently)
   end
 end
