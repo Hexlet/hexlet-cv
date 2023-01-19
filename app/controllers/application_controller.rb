f83d4abaaa137adc394fc5df42352327b25410a7
askor26 <askorutin26@gmail.com>
2023-01-20 02:24:20 +0300

    default url option added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,9 @@ class ApplicationController < ActionController::Base
 
     redirect_to("#{request.protocol}#{ENV.fetch('HOST')}#{request.fullpath}", allow_other_host: true, status: :moved_permanently)
   end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+  
 end
