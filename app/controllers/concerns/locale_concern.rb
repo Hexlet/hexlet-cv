7223143dbcb6296ba382f27b34da2a97abcc0694
askor26 <askorutin26@gmail.com>
2023-01-23 17:39:27 +0300

    locale concern added

---
+++ app/controllers/concerns/locale_concern.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module LocaleConcern
+    extend ActiveSupport::Concern
+  
+    included do
+      around_action :switch_locale
+    end
+  
+    def switch_locale(&)
+      locale = params[:locale]&.present? ? extract_locale : I18n.default_locale
+      I18n.with_locale(locale, &)
+    end
+  
+    def extract_locale
+      locale = params[:locale]
+      return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  
+      nil
+    end
+  end
+  
