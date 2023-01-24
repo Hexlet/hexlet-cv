9af61d0e418e767fec3f2b01d6822e7b2cf593a0
askor26 <askorutin26@gmail.com>
2023-01-24 20:59:35 +0300

    moved config options to environment/test.rb

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,8 +4,3 @@ class ApplicationMailer < ActionMailer::Base
   default from: Rails.application.config.vars[:email_from]
   layout 'mailer'
 end
-
-Rails.application.configure do
-  routes.default_url_options[:locale] = :en
-  config.action_mailer.default_url_options[:locale] = :en
-end
