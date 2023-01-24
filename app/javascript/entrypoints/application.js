d05cfa965fcd8b8cbffdf5da24628fb12ccf06e8
Maxim Novikov <maxim.novikov@onlinesup.com>
2023-01-24 22:30:18 +0800

    draft: create markdown editor by @toast-ui/editor

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -5,7 +5,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../stylesheets/application.scss';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from "@toast-ui/editor";
 
 import 'bootstrap-icons/font/bootstrap-icons.scss';
 // import '../scripts/selectVacancyFilter.js'
@@ -23,3 +26,24 @@ ujs.start();
 // const images = require.context('../images', true);
 // @ts-ignore
 // const imagePath = (name) => images(name, true)
+
+document.querySelectorAll('[data-provide="hexlet-markdown"]')
+    .forEach(el => {
+        const container = document.createElement('div')
+        el.parentElement.appendChild(container);
+        el.classList.add('d-none');
+        const editor = new Editor({
+            el: container,
+            initialEditType: 'markdown',
+            previewStyle: 'tab',
+            usageStatistics: false,
+            initialValue: el.value,
+            events: {
+                keyup(_, e) {
+                    el.value = e.target.outerText
+                }
+            }
+        });
+
+        editor.getMarkdown();
+    });
