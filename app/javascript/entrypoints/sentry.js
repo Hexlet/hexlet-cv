f39e5b484d9ae1ab04862757034792ec9a94de6f
Andrey Groza <grozwalker@gmail.com>
2023-01-27 13:30:38 +0000

    disable sentry debug mode

--- app/javascript/entrypoints/sentry.js
+++ app/javascript/entrypoints/sentry.js
@@ -5,30 +5,29 @@ import { BrowserTracing } from '@sentry/tracing';
 Sentry.init({
   dsn: 'https://85a2460d54d24846963a314a247a790a@o1090356.ingest.sentry.io/4504576077004800',
   integrations: [new BrowserTracing()],
-  debug: true,
   tracesSampleRate: 0.001,
-  // ignoreErrors: [
-  //   'top.GLOBALS', // Random plugins/extensions
-  //   'VK is',
-  //   'VK.Retargeting is ',
-  //   "Can't find variable: VK",
-  //   'pktAnnotationHighlighter',
-  //   'Unexpected keyword',
-  //   'illegal character',
-  //   'Unexpected identifier',
-  //   'Illegal invocation',
-  //   'missing = in const declaration',
-  // ],
+  ignoreErrors: [
+    'top.GLOBALS', // Random plugins/extensions
+    'VK is',
+    'VK.Retargeting is ',
+    "Can't find variable: VK",
+    'pktAnnotationHighlighter',
+    'Unexpected keyword',
+    'illegal character',
+    'Unexpected identifier',
+    'Illegal invocation',
+    'missing = in const declaration',
+  ],
   allowUrls: ['cv.hexlet.io'],
-  // beforeSend(event, hint) {
-  //   const stack = hint?.originalException?.stack || '';
-  //   const errorInitiator = stack
-  //     .split('\n')
-  //     .map((line) => line.trim())
-  //     .find((line) => line.startsWith('at'));
-  //   const causedByConsole = errorInitiator
-  //     ? errorInitiator.includes('<anonymous>:')
-  //     : false;
-  //   return causedByConsole ? null : event;
-  // },
+  beforeSend(event, hint) {
+    const stack = hint?.originalException?.stack || '';
+    const errorInitiator = stack
+      .split('\n')
+      .map((line) => line.trim())
+      .find((line) => line.startsWith('at'));
+    const causedByConsole = errorInitiator
+      ? errorInitiator.includes('<anonymous>:')
+      : false;
+    return causedByConsole ? null : event;
+  },
 });
