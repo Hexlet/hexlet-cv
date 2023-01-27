4ea16a45f29c3eb94ec61c974f739733b73934d2
Andrey Groza <grozwalker@gmail.com>
2023-01-27 12:58:02 +0000

    change sentry settings

--- app/javascript/entrypoints/sentry.js
+++ app/javascript/entrypoints/sentry.js
@@ -7,28 +7,28 @@ Sentry.init({
   integrations: [new BrowserTracing()],
   debug: true,
   tracesSampleRate: 0.001,
-  ignoreErrors: [
-    'top.GLOBALS', // Random plugins/extensions
-    'VK is',
-    'VK.Retargeting is ',
-    "Can't find variable: VK",
-    'pktAnnotationHighlighter',
-    'Unexpected keyword',
-    'illegal character',
-    'Unexpected identifier',
-    'Illegal invocation',
-    'missing = in const declaration',
-  ],
-  allowUrls: [/https?:\/\/cv.hexlet.io/],
-  beforeSend(event, hint) {
-    const stack = hint?.originalException?.stack || '';
-    const errorInitiator = stack
-      .split('\n')
-      .map((line) => line.trim())
-      .find((line) => line.startsWith('at'));
-    const causedByConsole = errorInitiator
-      ? errorInitiator.includes('<anonymous>:')
-      : false;
-    return causedByConsole ? null : event;
-  },
+  // ignoreErrors: [
+  //   'top.GLOBALS', // Random plugins/extensions
+  //   'VK is',
+  //   'VK.Retargeting is ',
+  //   "Can't find variable: VK",
+  //   'pktAnnotationHighlighter',
+  //   'Unexpected keyword',
+  //   'illegal character',
+  //   'Unexpected identifier',
+  //   'Illegal invocation',
+  //   'missing = in const declaration',
+  // ],
+  allowUrls: ['cv.hexlet.io'],
+  // beforeSend(event, hint) {
+  //   const stack = hint?.originalException?.stack || '';
+  //   const errorInitiator = stack
+  //     .split('\n')
+  //     .map((line) => line.trim())
+  //     .find((line) => line.startsWith('at'));
+  //   const causedByConsole = errorInitiator
+  //     ? errorInitiator.includes('<anonymous>:')
+  //     : false;
+  //   return causedByConsole ? null : event;
+  // },
 });
