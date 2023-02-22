292dac301b38c809da6cbb103e3e319f5bc7503b
usernaimandrey <anshlyapnikov@yandex.ru>
2023-02-22 17:55:09 +0300

    fix migration and add task

--- db/migrate/20230221221505_archive_vacancies_older_two_weeks.rb
+++ db/migrate/20230221221505_archive_vacancies_older_two_weeks.rb
@@ -2,8 +2,15 @@
 
 class ArchiveVacanciesOlderTwoWeeks < ActiveRecord::Migration[7.0]
   def change
-    Vacancy.find_each do |vacancy|
-      vacancy.archive! if vacancy.published? && vacancy.published_at < 2.weeks.ago
+    old_vacancies = Vacancy.where(published_at: ..2.weeks.ago)
+    old_vacancies.find_each do |vacancy|
+      vacancy.archive! if can_archive?(vacancy)
     end
+  end
+
+  private
+
+  def can_archive?(vacancy)
+    vacancy.valid? && vacancy.published?
   end
 end
