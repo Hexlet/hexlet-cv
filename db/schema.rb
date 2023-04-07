5f11d49b098da8ce0fb2521e5c9cc50337219cc3
Andrey Shlyapnikov <48034349+usernaimandrey@users.noreply.github.com>
2023-04-07 15:45:53 +0300

    Fix chenge user locale (#508)
    * update ruby v
    
    * fix-locale-in-resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_213157) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_122355) do
   create_table "countries", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
