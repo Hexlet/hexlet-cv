9981b7f4e71d9ad1ad87e5971efef195a85faa05
usernaimandrey <anshlyapnikov@yandex.ru>
2023-05-03 13:53:13 +0300

    fix

--- db/migrate/20230503095141_remove_uniq_index_from_career_items.rb
+++ db/migrate/20230503095141_remove_uniq_index_from_career_items.rb
@@ -1,4 +1,4 @@
-class RemoveUniqIndexToCareerItems < ActiveRecord::Migration[7.0]
+class RemoveUniqIndexFromCareerItems < ActiveRecord::Migration[7.0]
   def change
     remove_index :career_items, %i[career_id step_id]
   end
