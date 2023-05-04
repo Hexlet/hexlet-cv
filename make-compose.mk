beebb5f666e601376d7f36aa26f268fe554d34e8
Andrey Shlyapnikov <48034349+usernaimandrey@users.noreply.github.com>
2023-05-04 23:32:40 +0300

    Update make file (#545)
    * add model member
    
    * update make file
    
    * fix

--- make-compose.mk
+++ make-compose.mk
@@ -4,9 +4,6 @@ compose:
 compose-build:
 	docker-compose build
 
-compose-bash:
-	docker-compose run --rm app bash
-
 compose-clear:
 	docker-compose down -v --remove-orphans || true
 
@@ -16,29 +13,8 @@ compose-down:
 compose-install:
 	docker-compose run --rm app make setup
 
-compose-install-npm:
-	docker-compose run --rm app npm install --force
-
-compose-install-bundle:
-	docker-compose run --rm app bundle install --jobs $(shell nproc)
-
-compose-lint:
-	docker-compose run --rm app make lint
-
-compose-lint-fix:
-	docker-compose run --rm app make linter-code-fix
-
-compose-lint-eslint:
-	docker-compose run --rm app make lint-eslint
-
-compose-lint-eslint-fix:
-	docker-compose run --rm app make lint-eslint-fix
-
 compose-logs:
 	docker-compose logs -f
-
-compose-rails-console:
-	docker-compose run --rm app make console
 
 compose-restart:
 	docker-compose restart
@@ -47,12 +23,3 @@ compose-stop:
 	docker-compose stop || true
 
 compose-setup: compose-down compose-build compose-install
-
-compose-test:
-	docker-compose run --rm app make test
-
-compose-ci-check:
-	docker-compose run --rm app make ci-setup-check
-
-compose-rails:
-	docker-compose run --rm app bin/rails $(T)
