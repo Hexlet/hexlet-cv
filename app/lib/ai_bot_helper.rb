897b9d1cc9a5e49bc10f2dc7b007491bbe9fb9df
usernaimandrey <anshlyapnikov@yandex.ru>
2023-05-14 23:08:48 +0300

    fix

--- app/lib/ai_bot_helper.rb
+++ app/lib/ai_bot_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_litearl: true
-
 class AiBotHelper
   def self.ai_bot_user
     @ai_bot_user ||= User.find_by(email: ENV.fetch('EMAIL_SPECIAL_USER'))
