9c07ebcff61f9ef248aafbbf74b2ca5432a4c290
Andrey Shlyapnikov <48034349+usernaimandrey@users.noreply.github.com>
2023-05-16 11:26:11 +0300

    Migrete docker compose v2 (#567)
    * add model member
    
    * migrete to docker compose v2

--- make-services-app.mk
+++ make-services-app.mk
@@ -1,38 +1,38 @@
 app-bash:
-	docker-compose run --rm app bash
+	docker compose run --rm app bash
 
 app-install-npm:
-	docker-compose run --rm app npm install --force
+	docker compose run --rm app npm install --force
 
 app-install-bundle:
-	docker-compose run --rm app bundle install --jobs $(shell nproc)
+	docker compose run --rm app bundle install --jobs $(shell nproc)
 
 app-debug:
 	docker attach --sig-proxy=false --detach-keys="ctrl-c" $(shell docker ps -q --filter publish=3000)
 
 app-lint:
-	docker-compose run --rm app make lint
+	docker compose run --rm app make lint
 
 app-lint-fix:
-	docker-compose run --rm app make linter-code-fix
+	docker compose run --rm app make linter-code-fix
 
 app-lint-eslint:
-	docker-compose run --rm app make lint-eslint
+	docker compose run --rm app make lint-eslint
 
 app-lint-eslint-fix:
-	docker-compose run --rm app make lint-eslint-fix
+	docker compose run --rm app make lint-eslint-fix
 
 app-rails-console:
-	docker-compose run --rm app make console
+	docker compose run --rm app make console
 
 app-test:
-	docker-compose run --rm app make test
+	docker compose run --rm app make test
 
 app-ci-check:
-	docker-compose run --rm app make ci-setup-check
+	docker compose run --rm app make ci-setup-check
 
 app-rails:
-	docker-compose run --rm app bin/rails $(T)
+	docker compose run --rm app bin/rails $(T)
 
 app-make:
-	docker-compose run --rm app make $(T)
+	docker compose run --rm app make $(T)
