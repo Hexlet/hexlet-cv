5e812c67e48f72bfed9325121c6ace6ec69f2deb
usernaimandrey <anshlyapnikov@yandex.ru>
2023-05-17 11:07:48 +0300

    add item_oredr method in member controller

--- app/controllers/web/careers/members_controller.rb
+++ app/controllers/web/careers/members_controller.rb
@@ -8,14 +8,16 @@ class Web::Careers::MembersController < Web::Careers::ApplicationController
     @career_items = resource_career.items.order(order: :asc)
     @career_step_members = resource_member.career_step_members.ordered
 
-    current_career_item = resource_member.current_item
-
-    item_order = @career_items.find_by(order: params[:step])&.order ||
-                 current_career_item&.order ||
-                 @career_items.first.order
-
     @career_item = @career_items.find_by(order: item_order)
     @career_step = @career_item.career_step
     @career_step_member = resource_member.career_step_members.find_or_create_by!(career_step: @career_step)
+  end
+
+  private
+
+  def item_order
+    return @career_items[params[:step].to_i - 1].order if params[:step].present?
+
+    resource_member.current_item&.order || @career_items.first.order
   end
 end
