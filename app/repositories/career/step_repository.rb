294756891a4a7c376ccf73eeabbb9ade2aee6e97
usernaimandrey <anshlyapnikov@yandex.ru>
2023-06-02 20:35:02 +0300

    fix scope

--- app/repositories/career/step_repository.rb
+++ app/repositories/career/step_repository.rb
@@ -4,6 +4,6 @@ module Career::StepRepository
   extend ActiveSupport::Concern
 
   included do
-    scope :ordered, -> { includes(:career_items).merge(Career::Item.order(order: :asc)) }
+    scope :ordered, -> { includes(:career_items).merge(Career::Item.ordered) }
   end
 end
--- app/repositories/career_repository.rb
+++ app/repositories/career_repository.rb
@@ -4,7 +4,6 @@ module CareerRepository
   extend ActiveSupport::Concern
 
   included do
-    scope :without_archive_members, -> { where.not(career_members: { state: :archived }) }
     scope :for_user, ->(user) { joins(members: :user).where(members: { user: }) }
   end
 end
