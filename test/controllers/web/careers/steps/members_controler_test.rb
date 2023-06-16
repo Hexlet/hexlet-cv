41af31ab1ab9eeda549e4fcd3b077843875e1961
usernaimandrey <anshlyapnikov@yandex.ru>
2023-06-16 10:27:29 +0300

    add test on last step finished

--- test/controllers/web/careers/steps/members_controler_test.rb
+++ test/controllers/web/careers/steps/members_controler_test.rb
@@ -3,19 +3,37 @@
 class Web::Careers::Steps::MembersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @career = careers(:developer)
-    @step = career_steps(:step_one)
-    @career_step_member = career_step_members(:one)
   end
 
   test '#finish' do
+    step = career_steps(:step_one)
+    career_step_member = career_step_members(:one_by_user_full)
     user = users(:full)
     sign_in(user)
 
-    patch finish_career_step_member_path(@career, @step, @career_step_member)
+    patch finish_career_step_member_path(@career, step, career_step_member)
 
     assert_response :redirect
 
-    @career_step_member.reload
-    assert { @career_step_member.finished? }
+    career_step_member.reload
+    assert { career_step_member.finished? }
+  end
+
+  test '#finish last step' do
+    user = users(:one)
+    step = career_steps(:step_eight)
+    career_step_member = career_step_members(:eight_by_user_one)
+    career_member = career_members(:member_one)
+    sign_in(user)
+
+    patch finish_career_step_member_path(@career, step, career_step_member)
+
+    assert_response :redirect
+
+    career_step_member.reload
+    career_member = @career.members.find_by(user:)
+
+    assert { career_step_member.finished? }
+    assert { career_member.finished? }
   end
 end
