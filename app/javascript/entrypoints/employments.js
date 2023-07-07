d62ea55db2493c559649352f5b238e25f8abe337
Liza Chernyshova <liz4chernyshova@yandex.ru>
2023-07-07 10:14:28 +0000

    design CT landing

---
+++ app/javascript/entrypoints/employments.js
@@ -0,0 +1,35 @@
+import Swiper from 'swiper/bundle';
+
+const companies = new Swiper('#companies .swiper', {
+  loop: true,
+  slidesPerView: 2,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    992: {
+      slidesPerView: 6,
+    },
+    1200: {
+      slidesPerView: 8,
+    },
+  },
+});
+
+// const reviews = new Swiper('#reviews .swiper', {
+//   loop: true,
+//   autoplay: {
+//     delay: 5000,
+//   },
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   pagination: {
+//     el: '#reviews .swiper-pagination',
+//     clickable: true,
+//   },
+// });
