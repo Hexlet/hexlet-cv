b81d52b6a7b587bd583c4b71e67c12c8534b653d
usernaimandrey <anshlyapnikov@yandex.ru>
2023-07-18 10:49:07 +0300

    created lead

--- app/controllers/web/leads_controller.rb
+++ app/controllers/web/leads_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Web::LeadsController < Web::ApplicationController
-  def create; end
+  def create
+    @lead = Web::LeadForm.new(params[:web_lead_form])
+    if @lead.save
+      f(:success)
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Lead < ApplicationRecord
+  validates :email, :phone_number, :user_name, presence: true
 end
