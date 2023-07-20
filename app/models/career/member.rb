d787da73672ec4bc3fb26aa3408d1a0fa54e74f1
usernaimandrey <anshlyapnikov@yandex.ru>
2023-07-21 01:00:52 +0300

    refactoring career_member_users

--- app/models/career/member.rb
+++ app/models/career/member.rb
@@ -67,4 +67,12 @@ class Career::Member < ApplicationRecord
       career_step_members.finished.count
     end
   end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at finished_at state updated_at user_id]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[user career]
+  end
 end
