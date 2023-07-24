12de37df148ccc286d5a4ccc2aeed135d6fc28c4
usernaimandrey <anshlyapnikov@yandex.ru>
2023-07-24 10:41:44 +0300

    add comment

--- app/lib/event_sender.rb
+++ app/lib/event_sender.rb
@@ -5,6 +5,7 @@ class EventSender
     user = resource.user
     Event.create!(kind:, resource:, user:, locale: user.locale)
 
+    # NOTE: здесь будет job-a которая будет отправлять событие в n8n
     EmailSender.public_send("send_#{kind}_email", resource)
   end
 end
