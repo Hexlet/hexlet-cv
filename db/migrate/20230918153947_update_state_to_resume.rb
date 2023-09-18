42e4bdace73cbc3fad577577e9526cab3f6a80b5
usernaimandrey <anshlyapnikov@yandex.ru>
2023-09-18 23:54:36 +0300

    fix migration again

--- db/migrate/20230918153947_update_state_to_resume.rb
+++ db/migrate/20230918153947_update_state_to_resume.rb
@@ -1,9 +1,9 @@
 class UpdateStateToResume < ActiveRecord::Migration[7.0]
   def change
-    resume = Resume.find_by(id: 3067)
+    resume = Resume.find_by(id: 3068)
 
     return unless resume
 
-    resume.mark_as_failed!
+    resume.mark_as_failed! if resume.may_mark_as_failed?
   end
 end
--- db/migrate/20230918155843_update_state_to_resume_user.rb
+++
@@ -1,9 +0,0 @@
-class UpdateStateToResumeUser < ActiveRecord::Migration[7.0]
-  def change
-    resume = Resume.find_by(id: 3068)
-
-    return unless resume
-
-    resume.mark_as_failed!
-  end
-end
