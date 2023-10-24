1f50c31ab32c6e006857384020b8f8fa1482ee95
Kirill qsimpleq Babikhin <kirill.babikhin@gmail.com>
2023-10-24 18:15:46 +0500

    Update configs and scripts for render.com deploy

--- bin/render_lib.sh
+++ bin/render_lib.sh
@@ -6,7 +6,7 @@ function load_dotenv_file() {
   current_dir="$(pwd)";
   script_dir="$(dirname $0)";
   cd "$script_dir"
-  export DOTENV_FILE='/etc/secrets/env'
+  export DOTENV_FILE='/etc/secrets/.env'
   if [ -f "$DOTENV_FILE" ]; then export $(sed '/^\s*$/d; /^[[:space:]]*#/d' "$DOTENV_FILE"); fi
   cd $current_dir
 }
