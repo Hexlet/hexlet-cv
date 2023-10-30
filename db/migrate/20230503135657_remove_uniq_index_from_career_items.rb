4c2ba061389e0c732be111a4c3f6e986f474b092
Kirill Babikhin <kirill.babikhin@gmail.com>
2023-10-30 18:51:21 +0500

    Drop broken migrations (#692)

--- db/migrate/20230503135657_remove_uniq_index_from_career_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUniqIndexFromCareerItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :career_items, %i[career_id step_id]
-  end
-end
--- db/migrate/20230503140032_drop_table_steps.rb
+++
@@ -1,5 +0,0 @@
-class DropTableSteps < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :steps
-  end
-end
