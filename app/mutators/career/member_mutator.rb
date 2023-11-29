607a36880dd6b4ffa1557e7c73502b3b922fff97
usernaimandrey <anshlyapnikov@yandex.ru>
2023-11-29 17:20:26 +0300

    fix create career_member

--- app/mutators/career/member_mutator.rb
+++ app/mutators/career/member_mutator.rb
@@ -4,11 +4,10 @@ class Career::MemberMutator
   def self.create(career, params)
     career_member = career.members.build(params)
     user = career_member.user
-    ActiveRecord::Base.transaction do
-      career_member.save!
+
+    if career_member.save
       user.notifications.create!(kind: :new_career_member, resource: career_member)
     end
-
     career_member
   end
 end
