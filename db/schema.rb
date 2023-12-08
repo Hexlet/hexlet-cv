031d6d12813ae5d8ca1114222dcfac3dd2abf1d0
Kirill Chertkov <chertkov.k@gmail.com>
2023-12-08 15:55:16 +0300

    create migration add_not_null_constrain_column_name_to_resume

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_162228) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_124808) do
   create_table "career_items", force: :cascade do |t|
     t.integer "order"
     t.integer "career_id", null: false
@@ -208,7 +208,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_162228) do
 
   create_table "resumes", force: :cascade do |t|
     t.string "state"
-    t.string "name"
+    t.string "name", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
