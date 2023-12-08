7a79f242aa63b5f0642e6d667d43fb0ba32a5fc4
usernaimandrey <anshlyapnikov@yandex.ru>
2023-12-08 13:41:57 +0300

    add mark_as_outdated

---
+++ config/initializers/active_record.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module OutdatedAttributes
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def mark_as_outdated(*attributes)
+      @outdated_attributes = Set.new(attributes.map(&:to_s))
+      rewrite_outdated_getters
+      rewrite_outdated_setters
+    end
+
+    def outdated_attributes
+      @outdated_attributes || Set.new
+    end
+
+    def rewrite_outdated_getters
+      outdated_attributes.each do |attribute|
+        define_method attribute.to_s do
+          raise "Attribute #{attribute} for class '#{self.class}' was marked as outdated"
+        end
+      end
+    end
+
+    def rewrite_outdated_setters
+      outdated_attributes.each do |attribute|
+        define_method "#{attribute}=" do |_value|
+          raise "Attribute #{attribute} for class '#{self.class}' was marked as outdated"
+        end
+      end
+    end
+  end
+end
+
+class ActiveRecord::Base
+  include OutdatedAttributes
+end
