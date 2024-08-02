b7a2b93efba88bfb19a3cf2742b75df91ac94417
Nikolay Gagarinov <feycot@gmail.com>
2024-08-02 19:56:18 +0500

    fix wicked pdf for testing

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -15,7 +15,10 @@ WickedPdf.configure do |c|
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  c.exe_path = Gem.bin_path('wkhtmltopdf-heroku', 'wkhtmltopdf-binary', 'wkhtmltopdf')
+  #
+  bin_path = Rails.env.production? ? 'wkhtmltopdf-heroku' : 'wkhtmltopdf-binary'
+
+  c.exe_path = Gem.bin_path(bin_path, 'wkhtmltopdf')
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
