986a21d2436dfeac4245dd1f65a5e9225c4d75b9
Andrey Moshkov <fakesupp@gmail.com>
2024-08-15 11:05:27 +0000

    fix search on deleting users

--- app/controllers/api_internal/users_controller.rb
+++ app/controllers/api_internal/users_controller.rb
@@ -4,7 +4,7 @@ class ApiInternal::UsersController < ApiInternal::ApplicationController
   before_action :authenticate_admin!
 
   def search
-    search = User.ransack(first_name_or_last_name_or_email_cont: params[:term])
+    search = User.web.ransack(first_name_or_last_name_or_email_cont: params[:term])
     @users = search.result.limit(20)
   end
 end
