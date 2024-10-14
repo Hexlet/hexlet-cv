8cc8a39fb7fe1e56305d0abcb74f2006740efb12
Alexander Veselov <collabsis.g@gmail.com>
2024-10-14 11:02:30 +0400

    Added localize method for aasm state

--- app/helpers/model_helper.rb
+++ app/helpers/model_helper.rb
@@ -4,4 +4,11 @@ module ModelHelper
   def states_for_select(model, state_machine = :state)
     model.aasm(state_machine).states.map { |s| [s.human_name, s.name] }
   end
+
+  def state_for_render(model, state_machine = :state, state_name = :default)
+    model.aasm(state_machine)
+         .states
+         .find { |s| s.name == state_name.to_sym }
+         .try(:human_name)
+  end
 end
