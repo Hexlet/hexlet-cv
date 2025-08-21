1947434d207f0cb2e7dec67888e2089ffb69700a
Dangerwind <dangerwind@gmail.com>
2025-08-21 15:22:48 +0300

    add make ci-setup

--- src/main/java/io/hexlet/cv/controller/LoginController.java
+++ src/main/java/io/hexlet/cv/controller/LoginController.java
@@ -1,22 +1,18 @@
 package io.hexlet.cv.controller;
 
 import io.github.inertia4j.spring.Inertia;
-import io.hexlet.cv.config.CookieProperties;
 import io.hexlet.cv.dto.user.LoginRequestDTO;
 import io.hexlet.cv.dto.user.LoginResponseDTO;
 import io.hexlet.cv.security.TokenCookieService;
 import io.hexlet.cv.security.TokenService;
 import io.hexlet.cv.service.LoginService;
-import io.hexlet.cv.util.JWTUtils;
 import jakarta.validation.Valid;
 import java.util.Map;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
