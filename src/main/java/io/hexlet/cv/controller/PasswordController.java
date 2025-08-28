ca517e46cc3c82cb372b9e1fd59b86820414a143
Sanapol <junrey@list.ru>
2025-08-28 08:21:18 +0300

    add catch exception

--- src/main/java/io/hexlet/cv/controller/PasswordController.java
+++ src/main/java/io/hexlet/cv/controller/PasswordController.java
@@ -2,9 +2,9 @@ package io.hexlet.cv.controller;
 
 import io.github.inertia4j.spring.Inertia;
 import io.hexlet.cv.dto.user.UserPasswordDto;
-import io.hexlet.cv.service.PasswordService;
+import io.hexlet.cv.handler.exception.MatchingPasswordsException;
+import io.hexlet.cv.handler.exception.WrongPasswordException;
 import io.hexlet.cv.service.UserService;
-import jakarta.persistence.Access;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,19 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/users")
 @AllArgsConstructor
-public class PasswordController {
+public class UserController {
 
     private final Inertia inertia;
-    private final PasswordService passwordService;
+    private final UserService userService;
 
     @GetMapping(path = "/password")
-    public ResponseEntity<?> getChangeAccountPassword() {
-        ResponseEntity<?> response = inertia.render("/users/password");
+    public ResponseEntity<?> getChangeAccountPassword(@Valid @RequestBody UserPasswordDto userPasswordDto) {
+        ResponseEntity<?> response = inertia.render(
+                "/users/password", Map.of("password", userPasswordDto));
         return ResponseEntity.status(HttpStatus.OK)
                 .headers(response.getHeaders())
                 .body(response.getBody());
@@ -34,7 +36,15 @@ public class PasswordController {
 
     @PostMapping(path = "/password")
     public ResponseEntity<?> ChangeAccountPassword(@Valid @RequestBody UserPasswordDto userPasswordDto) {
-        passwordService.passwordChange(userPasswordDto);
+        try {
+            userService.passwordChange(userPasswordDto);
+        } catch (MatchingPasswordsException | WrongPasswordException e) {
+            ResponseEntity<?> response = inertia.render("/users/password",
+                    Map.of("password", userPasswordDto, "exception", e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .headers(response.getHeaders())
+                    .body(response.getBody());
+        }
         ResponseEntity<?> response = inertia.render("/users");
         return ResponseEntity.status(HttpStatus.OK)
                 .headers(response.getHeaders())
