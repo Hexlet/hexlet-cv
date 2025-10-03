1aaf4ace3432f455bfa1cb13b14b7e09c56ed326
walpurr <lukyanovalexey1@gmail.com>
2025-10-03 18:52:50 +0500

    create ResourceNotFoundException + put repeating code in handler to one method

--- src/main/java/io/hexlet/cv/handler/GlobalExceptionHandler.java
+++ src/main/java/io/hexlet/cv/handler/GlobalExceptionHandler.java
@@ -1,11 +1,10 @@
 package io.hexlet.cv.handler;
 
 import io.hexlet.cv.handler.exception.InvalidPasswordException;
+import io.hexlet.cv.handler.exception.ResourceNotFoundException;
 import io.hexlet.cv.handler.exception.UserAlreadyExistsException;
 import io.hexlet.cv.handler.exception.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -15,8 +14,39 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private Object commonHandle(Map<String, String> errors,
+                                HttpServletRequest request,
+                                RedirectAttributes redirectAttributes,
+                                HttpStatus status) {
+
+        // Обработка AJAX-запроса (Inertia)
+        if ("true".equals(request.getHeader("X-Inertia"))) {
+            redirectAttributes.addFlashAttribute("errors", errors);
+            String referer = request.getHeader("Referer");
+            RedirectView redirectView = new RedirectView(referer != null ? referer : "/");
+            redirectView.setHttp10Compatible(false);
+            redirectView.setStatusCode(HttpStatus.SEE_OTHER);
+            return redirectView;
+        }
+
+        // Обработка обычного запроса
+        return ResponseEntity.status(status).body(Map.of("errors", errors));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public Object handleResourceNotFoundException(ResourceNotFoundException ex,
+                                                  HttpServletRequest request,
+                                                  RedirectAttributes redirectAttributes) {
+
+        Map<String, String> errors = Map.of("error", ex.getMessage());
+        return commonHandle(errors, request, redirectAttributes, HttpStatus.NOT_FOUND);
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleValidation(MethodArgumentNotValidException ex,
@@ -24,23 +54,11 @@ public class GlobalExceptionHandler {
                                    RedirectAttributes redirectAttributes) {
 
         Map<String, String> errors = new HashMap<>();
-
         for (FieldError error : ex.getBindingResult().getFieldErrors()) {
             errors.put(error.getField(), error.getDefaultMessage());
         }
-        // если заголовок "X-Inertia: true" то это Inertia
-        if ("true".equals(request.getHeader("X-Inertia"))) {
-            redirectAttributes.addFlashAttribute("errors", errors);
-            String referer = request.getHeader("Referer");
 
-            RedirectView redirectView = new RedirectView(referer != null ? referer : "/");
-            redirectView.setHttp10Compatible(false);
-            redirectView.setStatusCode(HttpStatus.SEE_OTHER);
-            return redirectView;
-        }
-
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(Map.of("errors", errors));
+        return commonHandle(errors, request, redirectAttributes, HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
@@ -49,19 +67,7 @@ public class GlobalExceptionHandler {
                                           RedirectAttributes redirectAttributes) {
 
         Map<String, String> errors = Map.of("email", ex.getMessage());
-
-        if ("true".equals(request.getHeader("X-Inertia"))) {
-            redirectAttributes.addFlashAttribute("errors", errors);
-            String referer = request.getHeader("Referer");
-
-            RedirectView redirectView = new RedirectView(referer != null ? referer : "/");
-            redirectView.setHttp10Compatible(false);
-            redirectView.setStatusCode(HttpStatus.SEE_OTHER);
-            return redirectView;
-        }
-
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(Map.of("errors", errors));
+        return commonHandle(errors, request, redirectAttributes, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
@@ -70,19 +76,7 @@ public class GlobalExceptionHandler {
                                      RedirectAttributes redirectAttributes) {
 
         Map<String, String> errors = Map.of("email", ex.getMessage());
-
-        if ("true".equals(request.getHeader("X-Inertia"))) {
-            redirectAttributes.addFlashAttribute("errors", errors);
-            String referer = request.getHeader("Referer");
-
-            RedirectView redirectView = new RedirectView(referer != null ? referer : "/");
-            redirectView.setHttp10Compatible(false);
-            redirectView.setStatusCode(HttpStatus.SEE_OTHER);
-            return redirectView;
-        }
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("errors", errors));
+        return commonHandle(errors, request, redirectAttributes, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
@@ -91,18 +85,7 @@ public class GlobalExceptionHandler {
                                                  RedirectAttributes redirectAttributes) {
 
         Map<String, String> errors = Map.of("password", ex.getMessage());
-
-        if ("true".equals(request.getHeader("X-Inertia"))) {
-            redirectAttributes.addFlashAttribute("errors", errors);
-            String referer = request.getHeader("Referer");
-
-            RedirectView redirectView = new RedirectView(referer != null ? referer : "/");
-            redirectView.setHttp10Compatible(false);
-            redirectView.setStatusCode(HttpStatus.SEE_OTHER);
-            return redirectView;
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(Map.of("errors", errors));
+        return commonHandle(errors, request, redirectAttributes, HttpStatus.UNAUTHORIZED);
     }
 
 // это просто ошибки все остальное
