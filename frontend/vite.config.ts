e64ccb392e61c59b5d639a0ea49a36611576d151
Олеся Сыртина <olesademcenko87091@gmail.com>
2025-10-07 21:37:43 +0300

    Инициализация фронтенда + окно входа(login)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '^/[a-z]{2}(/|$)': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/api': 'http://localhost:8080',
+    },
+  },
 })
