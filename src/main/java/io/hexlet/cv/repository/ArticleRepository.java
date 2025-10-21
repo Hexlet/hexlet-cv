b189c15d24b10d4dcf78e871a1fe0fba31a92c54
Alexey Kiselev <alexey4050@mail.ru>
2025-10-21 10:32:04 +0300

    refactoring(articles): remove unused repository methods in ArticleRepository

--- src/main/java/io/hexlet/cv/repository/ArticleRepository.java
+++ src/main/java/io/hexlet/cv/repository/ArticleRepository.java
@@ -1,16 +1,13 @@
 package io.hexlet.cv.repository;
 
 import io.hexlet.cv.model.marketing.Article;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findAllByOrderByCreatedAtDesc();
 
     List<Article> findByShowOnHomepageTrueOrderByDisplayOrderAsc();
-
-    List<Article> findByHomeComponentIdOrderByDisplayOrderAsc(String homeComponentId);
 }
