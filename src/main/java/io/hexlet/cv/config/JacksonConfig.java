2f5fa866d5585dd83502f10169a007ca531588f2
Alexey Kiselev <alexey4050@mail.ru>
2025-10-21 10:29:44 +0300

    feat(config): add Jackson and Inertia configuration, serialization for dates

---
+++ src/main/java/io/hexlet/cv/config/JacksonConfig.java
@@ -0,0 +1,25 @@
+package io.hexlet.cv.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+
+        return mapper;
+    }
+}
