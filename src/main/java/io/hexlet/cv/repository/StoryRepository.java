9f436ce249b3653ae712a0ac8f4accf1512e1ca1
Alexey Kiselev <alexey4050@mail.ru>
2025-10-22 11:10:44 +0300

    style: fix checkstyle issues in StoryRepository

--- src/main/java/io/hexlet/cv/repository/StoryRepository.java
+++ src/main/java/io/hexlet/cv/repository/StoryRepository.java
@@ -1,11 +1,10 @@
 package io.hexlet.cv.repository;
 
 import io.hexlet.cv.model.marketing.Story;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, Long> {
