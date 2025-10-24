ebaa8cde106820dc812590408fe50f6b348e2163
Alexey Kiselev <alexey4050@mail.ru>
2025-10-24 16:10:21 +0300

    feat(team): add Team entity

---
+++ src/main/java/io/hexlet/cv/model/marketing/Team.java
@@ -0,0 +1,62 @@
+package io.hexlet.cv.model.marketing;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "marketing_team")
+@EntityListeners(AuditingEntityListener.class)
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String siteRole;
+
+    @Column(nullable = false)
+    private String systemRole;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(nullable = false)
+    private Boolean showOnHomepage;
+
+    private Integer displayOrder;
+
+    @Column(nullable = false)
+    private Boolean isPublished = false;
+
+    private LocalDateTime publishedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
