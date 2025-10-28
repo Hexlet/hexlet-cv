f16a9f181e4dfcf8d8cafe1c475bbcf09036003b
Alexander Vinogradow <alukard981@gmail.com>
2025-10-28 17:44:51 +0300

    refactor: in InterviewService remove methods without pagination in arguments, search method now receives String except for DTO, method for update Interview now uses custom created method in InterviewMapper [iss 906].

--- src/main/java/io/hexlet/cv/service/InterviewService.java
+++ src/main/java/io/hexlet/cv/service/InterviewService.java
@@ -2,7 +2,6 @@ package io.hexlet.cv.service;
 
 import io.hexlet.cv.dto.interview.InterviewCreateDTO;
 import io.hexlet.cv.dto.interview.InterviewDTO;
-import io.hexlet.cv.dto.interview.InterviewQueryParamsDTO;
 import io.hexlet.cv.dto.interview.InterviewUpdateDTO;
 import io.hexlet.cv.handler.exception.ResourceNotFoundException;
 import io.hexlet.cv.mapper.InterviewMapper;
@@ -15,9 +14,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
 public class InterviewService {
@@ -25,28 +21,13 @@ public class InterviewService {
     private final InterviewMapper interviewMapper;
     private final InterviewSpecification interviewSpecification;
 
-    public List<InterviewDTO> getAll() {
-        return interviewRepository.findAll().stream()
-                .map(interviewMapper::map)
-                .collect(Collectors.toList());
-    }
-
     public Page<InterviewDTO> getAll(Pageable pageable) {
         return interviewRepository.findAll(pageable)
                 .map(interviewMapper::map);
     }
 
-    public List<InterviewDTO> search(InterviewQueryParamsDTO interviewQueryParamsDTO) {
-        Specification<Interview> spec = interviewSpecification.build(interviewQueryParamsDTO.getInterviewSearchWord());
-        List<Interview> interviewsFromRepo = interviewRepository.findAll(spec);
-
-        return interviewsFromRepo.stream()
-                .map(interviewMapper::map)
-                .collect(Collectors.toList());
-    }
-
-    public Page<InterviewDTO> search(InterviewQueryParamsDTO interviewQueryParamsDTO, Pageable pageable) {
-        Specification<Interview> spec = interviewSpecification.build(interviewQueryParamsDTO.getInterviewSearchWord());
+    public Page<InterviewDTO> search(String searchWord, Pageable pageable) {
+        Specification<Interview> spec = interviewSpecification.build(searchWord);
 
         return interviewRepository.findAll(spec, pageable)
                 .map(interviewMapper::map);
@@ -70,7 +51,7 @@ public class InterviewService {
     public InterviewDTO update(InterviewUpdateDTO updateDTO, Long id) {
         Interview model = interviewRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Interview with id: " + id + " not found."));
-        interviewMapper.update(updateDTO, model);
+        interviewMapper.updateInterview(updateDTO, model);
         interviewRepository.save(model);
         InterviewDTO dto = interviewMapper.map(model);
 
