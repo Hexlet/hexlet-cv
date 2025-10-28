d0959bb6dbf2a1cae601ad4ee0bddbe1fc7e2860
Alexander Vinogradow <alukard981@gmail.com>
2025-10-28 17:47:55 +0300

    feat: create UserInterviewSummary dto to show info about speaker in Interview [iss 906].

---
+++ src/main/java/io/hexlet/cv/dto/interview/UserInterviewSummary.java
@@ -0,0 +1,12 @@
+package io.hexlet.cv.dto.interview;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInterviewSummary {
+    private Long id;
+    private String firstName;
+    private String lastname;
+}
