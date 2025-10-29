e8325d64d963b9550f5b46e81729ec37298be16a
Alexander Vinogradow <alukard981@gmail.com>
2025-10-29 21:36:31 +0300

    refactor: replace UserInterviewSummary with UserDTO entity + add userId in InterviewDTO [iss 906].

--- src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
+++ src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
@@ -1,13 +1,15 @@
 package io.hexlet.cv.dto.interview;
 
+import io.hexlet.cv.dto.user.UserDTO;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class InterviewDTO {
+    private Long id;
     private String title;
-    private UserInterviewSummary userInterviewSummary;
+    private UserDTO speaker;
     private String videoLink;
     private Boolean isPublished;
 }
