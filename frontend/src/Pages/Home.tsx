ffa64356348273f5974cc3add057b394be457b0d
Sergei Garankin <gudri@bk.ru>
2025-11-10 17:16:20 +0300

    add AboutUs section to Home component

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,21 +1,25 @@
 import { Link } from '@inertiajs/react'
 import { Container, Button, Group, Title } from '@mantine/core'
+import AboutUs from './AboutUs'
 
 export default function Home() {
   return (
-    <Container size="sm" mt="xl" ta="center">
-      <Title order={2} mb="xl">
-        Добро пожаловать!
-      </Title>
+    <>
+      <Container size="sm" mt="xl" ta="center">
+        <Title order={2} mb="xl">
+          Добро пожаловать!
+        </Title>
 
-      <Group justify="center">
-        <Button component={Link} href="/en/users/sign_in">
-          Вход
-        </Button>
-        <Button component={Link} href="/en/users/sign_up" variant="outline">
-          Регистрация
-        </Button>
-      </Group>
-    </Container>
+        <Group justify="center">
+          <Button component={Link} href="/en/users/sign_in">
+            Вход
+          </Button>
+          <Button component={Link} href="/en/users/sign_up" variant="outline">
+            Регистрация
+          </Button>
+        </Group>
+      </Container>
+      <AboutUs />
+    </>
   )
 }
