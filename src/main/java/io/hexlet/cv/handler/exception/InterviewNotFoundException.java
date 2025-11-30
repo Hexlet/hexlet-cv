6412097a1924e23b6705b5ff7b8075e7ddcbb7a5
Alexander Vinogradow <alukard981@gmail.com>
2025-11-30 20:06:06 +0300

    refactor: InterviewNotFoundException.java class created and added in GlobalExceptionHandler [iss 906].

---
+++ src/main/java/io/hexlet/cv/handler/exception/InterviewNotFoundException.java
@@ -0,0 +1,11 @@
+package io.hexlet.cv.handler.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InterviewNotFoundException extends RuntimeException{
+    public InterviewNotFoundException(String message) {
+        super(message);
+    }
+}
