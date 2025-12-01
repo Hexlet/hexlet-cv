56e8f4f4bd4ac658053ea04fe148abcc406c21e8
Alexander Vinogradow <alukard981@gmail.com>
2025-12-02 00:31:09 +0300

    refactor: checkstyle fix in InterviewNotFoundException [iss 906].

--- src/main/java/io/hexlet/cv/handler/exception/InterviewNotFoundException.java
+++ src/main/java/io/hexlet/cv/handler/exception/InterviewNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class InterviewNotFoundException extends RuntimeException{
+public class InterviewNotFoundException extends RuntimeException {
     public InterviewNotFoundException(String message) {
         super(message);
     }
