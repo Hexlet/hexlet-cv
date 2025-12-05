2e156ba1f9ec7619d78181231894a3ee1b72abba
Pavel Kochetkov <kochetkov_p@mail.ru>
2025-12-05 20:28:06 +0300

    temp: archive old code, add placeholder on homepage

---
+++ frontend/src/toDelete/Home.tsx
@@ -0,0 +1,24 @@
+import { Link } from '@inertiajs/react';
+import { Container, Button, Group, Title } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+
+export default function Home() {
+  const { t } = useTranslation();
+
+  return (
+    <Container size="sm" mt="xl" ta="center">
+      <Title order={2} mb="xl">
+        {t('homePage.greetings')}
+      </Title>
+
+      <Group justify="center">
+        <Button component={Link} href="/en/users/sign_in">
+          Вход
+        </Button>
+        <Button component={Link} href="/en/users/sign_up" variant="outline">
+          Регистрация
+        </Button>
+      </Group>
+    </Container>
+  );
+}
