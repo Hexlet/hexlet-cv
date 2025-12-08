b6dafe5383015e838434507175a5842b4f382ff1
Alexander Vinogradow <alukard981@gmail.com>
2025-12-08 23:40:51 +0300

    refactor: builder annotation added in InterviewDTO [iss 906].

--- src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
+++ src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
@@ -1,11 +1,13 @@
 package io.hexlet.cv.dto.interview;
 
 import io.hexlet.cv.dto.user.UserDTO;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class InterviewDTO {
     private Long id;
     private String title;
