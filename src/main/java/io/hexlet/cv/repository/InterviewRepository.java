793024c8eabf470d8fbcc582c85cd7ad00fae78d
Alexander Vinogradow <alukard981@gmail.com>
2025-12-09 23:16:13 +0300

    refactor: method findByTitle created in InterviewRepository [iss 906].

--- src/main/java/io/hexlet/cv/repository/InterviewRepository.java
+++ src/main/java/io/hexlet/cv/repository/InterviewRepository.java
@@ -1,10 +1,12 @@
 package io.hexlet.cv.repository;
 
 import io.hexlet.cv.model.Interview;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview, Long>, JpaSpecificationExecutor<Interview> {
+    Optional<Interview> findByTitle(String title);
 }
