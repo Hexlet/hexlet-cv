c10478d0ed026e41e00436f893a2f645dba9f715
Alexander Vinogradow <alukard981@gmail.com>
2025-12-09 23:13:32 +0300

    refactor: added createdAt field in InterviewDTO [iss 906].

--- src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
+++ src/main/java/io/hexlet/cv/dto/interview/InterviewDTO.java
@@ -1,6 +1,8 @@
 package io.hexlet.cv.dto.interview;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.hexlet.cv.dto.user.UserDTO;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +16,7 @@ public class InterviewDTO {
     private UserDTO speaker;
     private String videoLink;
     private Boolean isPublished;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime createdAt;
 }
