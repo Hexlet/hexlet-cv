38404eb3f42862e28e669ee02e3861f1cfaab346
Sergei Garankin <gudri@bk.ru>
2025-12-14 21:39:31 +0300

    add i18n support to WhoWeAre section

--- frontend/src/app/providers/i18n/locales/en.ts
+++ frontend/src/app/providers/i18n/locales/en.ts
@@ -2,6 +2,19 @@ export default {
   translation: {
     homePage: {
       greetings: 'Welcome',
+      whoWeAre: {
+        title: 'Hello, we are Hexlet',
+        subtitle: 'ecosystem for starting and developing in IT:',
+        features: {
+          resume: 'Create a resume',
+          apply: 'Apply for jobs',
+          searchJobs: 'Search for vacancies and internships',
+          chatRecruiters: 'Chat with recruiters',
+          coverLetters: 'Write cover letters',
+          prepareInterviews: 'Prepare for interviews',
+          getExperience: 'Get commercial experience',
+        },
+      },
     },
   }
 };
--- frontend/src/app/providers/i18n/locales/ru.ts
+++ frontend/src/app/providers/i18n/locales/ru.ts
@@ -2,6 +2,19 @@ export default {
   translation: {
     homePage: {
       greetings: 'Добро пожаловать!',
+      whoWeAre: {
+        title: 'Привет, мы Хекслет',
+        subtitle: 'экосистема для старта и развития в IT:',
+        features: {
+          resume: 'Составлять резюме',
+          apply: 'Откликаться',
+          searchJobs: 'Искать вакансии и стажировки',
+          chatRecruiters: 'Переписываться с рекрутерами',
+          coverLetters: 'Писать сопроводительные',
+          prepareInterviews: 'Готовить интервью',
+          getExperience: 'Получать коммерческий опыт',
+        },
+      },
     },
   }
 };
