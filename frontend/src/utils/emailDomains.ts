f10a07325d032799c7bf6f7ca85736fdc223fb9a
immortal-p <immortalkil4oy@gmail.com>
2025-12-15 15:13:49 +0300

    feat(auth): Adding a file with a blacklist of domains

---
+++ frontend/src/utils/emailDomains.ts
@@ -0,0 +1,5 @@
+export const BLOCKED_DOMAINS = [
+    "10minutemail.com", "mailinator.com", "tempmail.com", 
+    "guerrillamail.com", "yopmail.com", "trashmail.com", 
+    "dispostable.com", "emailondeck.com", "fakeinbox.com"
+];
