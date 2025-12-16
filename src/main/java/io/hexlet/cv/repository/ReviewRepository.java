fddf4edb0d00adb48d3d5bb89746f49a6f716610
Alexey Kiselev <alexey4050@mail.ru>
2025-12-16 12:43:00 +0300

    refactor: update mappers, repositories and services

--- src/main/java/io/hexlet/cv/repository/ReviewRepository.java
+++ src/main/java/io/hexlet/cv/repository/ReviewRepository.java
@@ -2,15 +2,16 @@ package io.hexlet.cv.repository;
 
 import io.hexlet.cv.model.admin.marketing.Review;
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findAllByOrderByCreatedAtDesc();
 
-    List<Review> findByShowOnHomepageTrueOrderByDisplayOrderAsc();
+    Page<Review> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    Optional<Review> findByIdAndIsPublishedTrue(Long id);
+    List<Review> findByShowOnHomepageTrue(Sort sort);
 }
--- src/main/java/io/hexlet/cv/repository/StoryRepository.java
+++ src/main/java/io/hexlet/cv/repository/StoryRepository.java
@@ -2,16 +2,16 @@ package io.hexlet.cv.repository;
 
 import io.hexlet.cv.model.admin.marketing.Story;
 import java.util.List;
-import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, Long> {
 
-    List<Story> findAllByOrderByCreatedAtDesc();
+    Page<Story> findAllByOrderByCreatedAtDesc(Pageable pageable);
 
-    List<Story> findByShowOnHomepageTrueOrderByDisplayOrderAsc();
-
-    Optional<Story> findByIdAndIsPublishedTrue(Long id);
+    List<Story> findByShowOnHomepageTrue(Sort sort);
 }
