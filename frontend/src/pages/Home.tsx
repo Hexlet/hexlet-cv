06f7353e4cbccf6288df48806e953f9acd8aa5f7
Sergei Garankin <gudri@bk.ru>
2025-12-18 12:54:08 +0300

    add MarketAnalytics section to Home component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import { Center, Container, Title } from '@mantine/core';
 import { useTranslation } from 'react-i18next';
+import { MarketAnalytics } from '@widgets/market-analytics';
 
 export default function Home() {
   const { t } = useTranslation();
 
   return (
-    <Center h="100vh">
-      <Container 
-        ta='center'
-        c='orange'
-      >
-        <Title>
-          {t('homePage.greetings')} Я - главная страница!
-        </Title>
-      </Container>
-    </Center>
+    <>
+      <Center h="100vh">
+        <Container 
+          ta='center'
+          c='orange'
+        >
+          <Title>
+            {t('homePage.greetings')} Я - главная страница!
+          </Title>
+        </Container>
+      </Center>
+      <MarketAnalytics/>
+    </>
   );
 }
