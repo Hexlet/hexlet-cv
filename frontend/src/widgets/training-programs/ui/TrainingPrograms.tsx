6e188215081c123368916e0dc2f880764b874cbc
Sergei Garankin <gudri@bk.ru>
2025-12-27 15:39:39 +0300

    removed color attribures on Text

--- frontend/src/widgets/training-programs/ui/TrainingPrograms.tsx
+++ frontend/src/widgets/training-programs/ui/TrainingPrograms.tsx
@@ -6,22 +6,22 @@ export const TrainingPrograms: React.FC = () => {
 
     return (
         <Container size="lg" py="xs">
-            <Title order={1} c="white" fw="bold" mb="md">
+            <Title order={1} fw="bold" mb="md">
                 {t('homePage.trainingPrograms.title')}
             </Title>
             <Grid>
                 <Grid.Col span={{ base: 12, md: 4 }}>
                     <Card radius="lg" bg="dark.5" h="100%">
                         <Group gap="sm">
-                            <Text fz="h3" c="white">
+                            <Text fz="h3">
                                 {t('homePage.trainingPrograms.cards.jobSearch.title')}
                             </Text>
                         </Group>
-                        <Text size="sm" c="white" mb="lg" mt="xs">
+                        <Text size="sm" mb="lg" mt="xs">
                             {t('homePage.trainingPrograms.cards.jobSearch.description')}
                         </Text>
                         <Button radius="lg" color="white" w="fit-content">
-                            <Text c="dark" size="sm">
+                            <Text size="sm">
                                 {t('homePage.trainingPrograms.cards.jobSearch.button')}
                             </Text>
                         </Button>
@@ -30,15 +30,15 @@ export const TrainingPrograms: React.FC = () => {
                 <Grid.Col span={{ base: 12, md: 4 }}>
                     <Card radius="lg" bg="dark.5" h="100%">
                         <Group gap="sm">
-                            <Text fz="h3" c="white">
+                            <Text fz="h3">
                                 {t('homePage.trainingPrograms.cards.freelance.title')}
                             </Text>
                         </Group>
-                        <Text size="sm" c="white" mb="lg" mt="xs">
+                        <Text size="sm" mb="lg" mt="xs">
                             {t('homePage.trainingPrograms.cards.freelance.description')}
                         </Text>
                         <Button radius="lg" color="white" w="fit-content">
-                            <Text c="dark" size="sm">
+                            <Text size="sm">
                                 {t('homePage.trainingPrograms.cards.freelance.button')}
                             </Text>
                         </Button>
@@ -47,15 +47,15 @@ export const TrainingPrograms: React.FC = () => {
                 <Grid.Col span={{ base: 12, md: 4 }}>
                     <Card radius="lg" bg="dark.5" h="100%">
                         <Group gap="sm">
-                            <Text fz="h3" c="white">
+                            <Text fz="h3">
                                 {t('homePage.trainingPrograms.cards.foreignJobs.title')}
                             </Text>
                         </Group>
-                        <Text size="sm" c="white" mb="lg" mt="xs">
+                        <Text size="sm" mb="lg" mt="xs">
                             {t('homePage.trainingPrograms.cards.foreignJobs.description')}
                         </Text>
                         <Button radius="lg" color="white" w="fit-content">
-                            <Text c="dark" size="sm">
+                            <Text size="sm">
                                 {t('homePage.trainingPrograms.cards.foreignJobs.button')}
                             </Text>
                         </Button>
