69b87ae677cdf76dc74eaebd94ce9db6f3385bc4
nathalieMalsh <nmalysheva99@mail.ru>
2026-01-16 20:06:36 +0300

    add mocks study programs, style StudyPrograms widget

--- frontend/src/pages/Admin/Programs/Index.tsx
+++ frontend/src/pages/Admin/Programs/Index.tsx
@@ -1,13 +1,19 @@
 import { StudyPrograms } from "@widgets/study-programs"
 
-// Программы для демонстрации
-const mockPrograms = [
-    { id: 1, name: 'Frontend-разработчик', duration: 6, lessons: 48, isPublished: true },
-    { id: 2, name: 'QA-инженер', duration: 4, lessons: 32, isPublished: false }
-]
-
-const Index2 = () => {
-    return <StudyPrograms programs={mockPrograms} />
+type StudyProgramsEntry = {
+    id: number
+    name: string
+    duration: number
+    lessons: number
+    isPublished: boolean
 }
 
-export default Index2
+type TProps = {
+    programs: StudyProgramsEntry[]
+}
+
+const Index = ({ programs }: TProps) => {
+    return <StudyPrograms programs={programs} />
+}
+
+export default Index
